26c55edb5016c3af396d192688566ffb3eb13c02
GaelDuran <ldt76578@lasallebajio.edu.mx>
2025-05-12 16:09:35 -0600

    Implementacion de Retrofit y Funcionalidad para obtener animales y ambientes

--- app/src/main/java/com/example/animalsapp/service/AnimalService.kt
+++ app/src/main/java/com/example/animalsapp/service/AnimalService.kt
@@ -1,4 +1,28 @@
 package com.example.animalsapp.service
 
+import com.example.animalsapp.models.Animal
+import com.example.animalsapp.models.Environment
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+
 interface AnimalService {
+
+    @GET("animals")
+    suspend fun getAnimals(): List<Animal>
+
+    @GET("animals/{id}")
+    suspend fun getAnimalById( @Path("id") id:String) : Animal
+
+    @GET("animals")
+    suspend fun getAnimalByEnvironmentId(
+        @Query("environmentId") environmentId: String,
+    ) : List<Animal>
+
+    @GET("environments")
+    suspend fun getEnvironments(): List<Environment>
+
+    @GET("environments/{id}")
+    suspend fun getEnvironmentById ( @Path("id") id: String) : Environment
 }
