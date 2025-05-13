4fc81c425bf4b18ac186505f84167a28fd062210
GaelDuran <ldt76578@lasallebajio.edu.mx>
2025-05-12 19:49:47 -0600

    Archivo AnimalDetail completado

--- app/src/main/java/com/example/animalsapp/screens/AnimalDetailScreen.kt
+++ app/src/main/java/com/example/animalsapp/screens/AnimalDetailScreen.kt
@@ -1,8 +1,24 @@
 package com.example.animalsapp.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -10,6 +26,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.example.animalsapp.models.Animal
 import com.example.animalsapp.service.AnimalService
 import kotlinx.coroutines.launch
@@ -43,6 +69,102 @@ fun AnimalDetailScreen( innerPadding: PaddingValues, animalId: String){
         }
     }
     if (isLoading) {
-        Box() {  }
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        animal?.let { animal ->
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState())
+                    .padding(horizontal = 25.dp)
+            ) {
+                Text(
+                    text = animal.name,
+                    color = Color.White,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.padding(vertical = 10.dp))
+
+                AsyncImage(
+                    model = animal.image,
+                    contentDescription = animal.name,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .size(220.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.padding(vertical = 6.dp))
+
+                Text(
+                    text = animal.description,
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.padding(vertical = 6.dp))
+
+                Text(
+                    text = "Hechos interesantes",
+                    color = Color(0xFFAEB044),
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.padding(vertical = 6.dp))
+
+                animal.facts.forEach { fact ->
+                    FactItem(text = fact)
+                    Spacer(modifier = Modifier.padding(vertical = 3.dp))
+                }
+
+                Spacer(modifier = Modifier.padding(vertical = 6.dp))
+
+                Text(
+                    text = "Galeria de imagenes",
+                    color = Color(0xFFAEB044),
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.padding(vertical = 6.dp))
+
+                LazyRow (
+                    horizontalArrangement = Arrangement.spacedBy(6.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    items(animal.imageGallery) { image ->
+                        Box(
+                            modifier = Modifier
+                                .width(350.dp)
+                                .height(300.dp)
+                                .clip(RoundedCornerShape(30.dp))
+                        ) {
+                            AsyncImage(
+                                model = image,
+                                contentDescription = animal.name,
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
