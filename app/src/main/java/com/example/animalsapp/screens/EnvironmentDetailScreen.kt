2546deb1014edc0595b3125a577a3b720b6cdc47
GaelDuran <ldt76578@lasallebajio.edu.mx>
2025-05-12 20:07:39 -0600

    Archivo EnvironmentDetail completado

--- app/src/main/java/com/example/animalsapp/screens/EnvironmentDetailScreen.kt
+++ app/src/main/java/com/example/animalsapp/screens/EnvironmentDetailScreen.kt
@@ -1,2 +1,155 @@
 package com.example.animalsapp.screens
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+import com.example.animalsapp.models.Animal
+import com.example.animalsapp.models.Environment
+import com.example.animalsapp.service.AnimalService
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Composable
+fun EnvironmentDetailScreen(
+    innerPading : PaddingValues,
+    environmentId : String,
+    navController: NavController
+
+) {
+    var BASE_URL = "https://animals.juanfrausto.com/api/"
+    val scope = rememberCoroutineScope()
+    var environment by remember {
+        mutableStateOf<Environment?>(null)
+    }
+    var animalsEnvironment by remember {
+        mutableStateOf(listOf<Animal>())
+    }
+    var isLoading by remember {
+        mutableStateOf(true)
+    }
+
+    LaunchedEffect(key1 = true ) {
+        scope.launch {
+            try {
+                val retrofitBuilder = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                val animalService = retrofitBuilder.create(AnimalService::class.java)
+                environment = animalService.getEnvironmentById(environmentId)
+                animalsEnvironment = animalService.getAnimalByEnvironmentId(environmentId)
+                Log.i("EnvironmentDetailScreen", environment.toString())
+            }
+            catch (e: Exception){
+                Log.e("Error", e.toString())
+            }finally {
+                isLoading = false
+            }
+        }
+    }
+
+    if(isLoading){
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    }else{
+        environment?.let{
+            environment ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPading)
+                    .padding(vertical = 20.dp)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                AsyncImage(
+                    model = environment.image,
+                    contentDescription = environment.name,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp)
+                        .clip(RoundedCornerShape(32.dp))
+                        .padding(horizontal = 20.dp)
+                        .align(Alignment.CenterHorizontally),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = environment.name,
+                    color = Color.White,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(top = 12.dp),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = environment.description,
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                if (animalsEnvironment.isNotEmpty()) {
+                    animalsEnvironment.forEach { animal ->
+                        AnimalCard(
+                            animal = animal,
+                        ) { animal ->
+                            Log.i("AnimalEspecifico", animal.toString())
+                            navController.navigate("animal-list/${animal.id}")
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                    }
+
+                } else {
+                    Text(
+                        text = "No hay animales en este ambiente",
+                        color = Color.White,
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+
+    }
+}
